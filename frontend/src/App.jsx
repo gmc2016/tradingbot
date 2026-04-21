0b15ffc7cf032dd662ff0d012b2829fa99583156
gmc2016 <111246844+gmc2016@users.noreply.github.com>
2026-04-21 07:43:56 -0300

    Add files via upload

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import axios from 'axios'
 import { useAuth, useDashboard } from './hooks/useDashboard'
 import LoginPage      from './components/LoginPage'
 import Topbar         from './components/Topbar'
